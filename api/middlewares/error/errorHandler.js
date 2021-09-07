91dde8608652ee3ad1ee37255008dcc3e04c1432
feta_deven <deshdeven@gmail.com>
2021-09-06 20:41:56 -0700

    added error handler

--- api/middlewares/error/errorHandler.js
+++ api/middlewares/error/errorHandler.js
@@ -1,19 +1,19 @@
 // eslint-disable-next-line no-unused-vars
 const errorHandler = (err, req, res, next) => {
-    let error = {
-        ...err
-    };
-    error.message = err.message;
-    console.log('====================================');
-    console.log(error);
-    console.log('====================================');
+  let error = {
+    ...err,
+  };
+  error.message = err.message;
+  console.log("====================================");
+  console.log(error);
+  console.log("====================================");
 
-    if (!error.statusCode) error.message = "Sorry internal server error occured";
+  if (!error.statusCode) error.message = "Sorry internal server error occured";
 
-    res.status(error.statusCode || 500).json({
-        success: false,
-        error: error.message || "Sorry an internal server error occured",
-    })
-}
+  res.status(error.statusCode || 500).json({
+    success: false,
+    error: error.message || "Sorry an internal server error occured",
+  });
+};
 
-module.exports = errorHandler
+module.exports = errorHandler;

a0779dcda66810a7f5f1348ac641453be929f6a0
feta_deven <deshdeven@gmail.com>
2021-09-06 18:11:02 -0700

    added index routing file

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
-const express = require('express')
-require('dotenv').config({
-    path: '.env',
-})
-const cookieParser = require('cookie-parser')
+const express = require("express");
+require("dotenv").config({
+    path: ".env",
+});
+const cookieParser = require("cookie-parser");
+const router = require("./api/routes/index.routes");
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.set('view engine', 'ejs')
-
-app.use(express.static('public'))
+app.use(express.static("public"));
 app.use(express.json());
-app.use(express.urlencoded({
-    extended: true
-}))
-app.use(cookieParser())
+app.use(cookieParser());
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Printer Tracker Running on Port ${port}`))
+app.use("/api/v1", router);
+
+app.get("/", (req, res) => res.send("Hello World!"));
+app.listen(port, () => console.log(`Printer Tracker Running on Port ${port}`));

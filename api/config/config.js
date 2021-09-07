df8c7bedec3df7ecf3e4b7ca55a759dc9bef388b
feta_deven <deshdeven@gmail.com>
2021-09-06 23:34:31 -0700

    added hard coded config

---
+++ api/config/config.js
@@ -0,0 +1,35 @@
+const status = {
+  pending: 1,
+  approved: 2,
+};
+const permissions = {
+  //roles
+  can_add_roles: "can_add_roles",
+  can_edit_roles: "can_edit_roles",
+  can_delete_roles: "can_delete_roles",
+  can_view_roles: "can_view_roles",
+  can_approve_roles: "can_approve_roles",
+  //printer types
+  can_add_printer_types: "can_add_printer_types",
+  can_edit_printer_types: "can_edit_printer_types",
+  can_view_printer_types: "can_view_printer_types",
+  can_delete_printer_types: "can_delete_printer_types",
+  can_approve_printer_types: "can_approve_printer_types",
+  //printers
+  can_view_printers: "can_view_printers",
+  can_add_printers: "can_add_printers",
+  can_edit_printers: "can_edit_printers",
+  can_delete_printers: "can_view_printers",
+  can_approve_printers: "can_approve_printers",
+  can_export_printers: "can_export_printers",
+  //print outs
+  can_view_print_outs: "can_view_print_outs",
+  can_view_all_print_outs: "can_view_all_print_outs",
+  can_add_print_outs: "can_add_print_outs",
+  can_edit_print_outs: "can_edit_print_outs",
+  can_export_print_outs: "can_export_print_outs",
+  can_delete_print_outs: "can_delete_print_outs",
+  can_approve_print_outs: "can_approve_print_outs",
+};
+
+module.exports = { permissions, status };

2f44a26f5827af979ca5764faac1e976fee1947b
feta_deven <deshdeven@gmail.com>
2021-09-06 18:21:47 -0700

    created auth router

--- api/routes/index.routes.js
+++ api/routes/index.routes.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const authRouter = require('./auth/auth.routes');
 
 const router = express.Router();
 
+router.use("/auth", authRouter);
 
 module.exports = router

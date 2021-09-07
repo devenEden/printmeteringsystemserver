4c1124b971f2ca368cdc2f405aac45cbec4419c9
feta_deven <deshdeven@gmail.com>
2021-09-06 18:55:44 -0700

    created db connection

---
+++ api/config/db/connectToDb.js
@@ -0,0 +1,26 @@
+const { Pool } = require("pg");
+/**
+ * @description config for db connection
+ */
+const dbconfig = {
+  user: process.env.DB_USERNAME,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+};
+
+const localDbConfig = {
+  user: process.env.LOCAL_DB_USERNAME,
+  host: process.env.LOCAL_DB_HOST,
+  database: process.env.LOCAL_DB_DATABASE,
+  password: process.env.LOCAL_DB_PASSWORD,
+  port: process.env.LOCAL_DB_PORT,
+};
+/**
+ * connect to postgres database
+ * @instance of Pool module from pg dependence
+ */
+const pool = new Pool(localDbConfig);
+
+module.exports = pool;

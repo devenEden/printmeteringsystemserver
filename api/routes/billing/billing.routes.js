6c28c12eac2d55e67e7ea8146fb6b4d7106b2057
feta_deven <deshdeven@gmail.com>
2021-09-10 12:38:00 -0700

    added protect func to billing

--- api/routes/billing/billing.routes.js
+++ api/routes/billing/billing.routes.js
@@ -3,10 +3,12 @@ const {
   can_view_billing,
 } = require("../../config/permissions/permissions.config");
 const { getBilling } = require("../../controllers/billing/billing.controller");
+const protect = require("../../middlewares/auth/protect");
 const checkHasPermission = require("../../middlewares/permissions/permissions");
 
 const billingRouter = express.Router();
 
+billingRouter.use(protect);
 billingRouter.get(
   "/",
   checkHasPermission(can_view_billing).permission,

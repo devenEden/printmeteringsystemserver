ecef19e58ec5a794dcd0bffa48f10a98e5bd8665
feta_deven <deshdeven@gmail.com>
2021-09-13 08:52:22 -0700

    changed db connection env variables

--- api/config/db/connectToDb.js
+++ api/config/db/connectToDb.js
@@ -5,25 +5,25 @@ require("dotenv").config({
 /**
  * @description config for db connection
  */
-// const dbconfig = {
-//   user: process.env.DB_USERNAME,
-//   host: process.env.DB_HOST,
-//   database: process.env.DB_DATABASE,
-//   password: process.env.DB_PASSWORD,
-//   port: process.env.DB_PORT,
-// };
-
-const localDbConfig = {
-  user: process.env.LOCAL_DB_USERNAME,
-  host: process.env.LOCAL_DB_HOST,
-  database: process.env.LOCAL_DB_DATABASE,
-  password: process.env.LOCAL_DB_PASSWORD,
-  port: process.env.LOCAL_DB_PORT,
+const dbconfig = {
+  user: process.env.DB_USERNAME,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 };
+
+// const localDbConfig = {
+//   user: process.env.LOCAL_DB_USERNAME,
+//   host: process.env.LOCAL_DB_HOST,
+//   database: process.env.LOCAL_DB_DATABASE,
+//   password: process.env.LOCAL_DB_PASSWORD,
+//   port: process.env.LOCAL_DB_PORT,
+// };
 /**
  * connect to postgres database
  * @instance of Pool module from pg dependence
  */
-const pool = new Pool(localDbConfig);
+const pool = new Pool(dbconfig);
 
 module.exports = pool;
